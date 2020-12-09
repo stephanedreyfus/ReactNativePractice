5d5d5f8cc45728c6cc160e7777a7427b146c27ab
Stéphane Dreyfus <floatingrock@gmail.com>
2020-12-09 13:03:47 -0800

    Playing with props.

--- App.js
+++ App.js
@@ -2,20 +2,38 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'lightblue',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
+  },
+
+  greeting: {
+    // flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    top: 100,
   },
 });
+
+const Greeting = (props) => {
+  return (
+    <View style={styles.greeting}>
+      <Text>Hello {props.name}!</Text>
+    </View>
+  );
+}
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Greeting name='Nicholas' />
+      <Greeting name='Hieronymus' />
+      <Greeting name='Jessica' />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+export default App;
